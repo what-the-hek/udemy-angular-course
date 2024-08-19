e28b9e38aa33ef3fd81552922f9483617ba87d8d
what-the-hek <108735805+what-the-hek@users.noreply.github.com>
2024-08-19 11:03:25 +0800

    titlecase pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'udemy-angular-course';
+  name: string = '';
+
+  onNameChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.name = target.value;
+  }
+
 }
