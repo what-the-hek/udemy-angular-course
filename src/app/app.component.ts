5ce6c8892b609f2a05580779cd0522bbb4218bc9
what-the-hek <108735805+what-the-hek@users.noreply.github.com>
2024-08-21 17:20:11 +0800

    indexing page numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+
   currentPage = 0;
 
   images = [
