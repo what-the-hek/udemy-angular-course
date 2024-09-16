66e0c39d66afccffe84d7f9efca36287ce6e6b91
what-the-hek <108735805+what-the-hek@users.noreply.github.com>
2024-09-16 13:58:54 +0800

    limit page numbers in nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,6 +29,22 @@ export class AppComponent {
     {
       title: 'At the jungle',
       url: 'https://images.unsplash.com/photo-1470058869958-2a77ade41c02?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8anVuZ2xlfGVufDB8fDB8fHww',
-    }
+    },
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8fDA%3D',
+    },
+    {
+      title: 'In the forest',
+      url: 'https://images.unsplash.com/photo-1448375240586-882707db888b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9yZXN0fGVufDB8fDB8fHww',
+    },
+    {
+      title: 'In the bush',
+      url: 'https://images.unsplash.com/photo-1659611995451-9894cae72105?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXVzdHJhbGlhbiUyMGJ1c2h8ZW58MHx8MHx8fDA%3D',
+    },
+    {
+      title: 'At the jungle',
+      url: 'https://images.unsplash.com/photo-1470058869958-2a77ade41c02?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8anVuZ2xlfGVufDB8fDB8fHww',
+    },
   ]
 }
